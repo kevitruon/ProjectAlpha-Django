ebc30eefbc6d4f9bf29f890d5cf8bcf0fd2300b5
kevitruon <kevitruon@gmail.com>
2023-12-18 15:20:14 -0800

    Feature 5 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from projects.models import Project
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def list_projects(request):
+    projects = Project.objects.filter(owner = request.user)
+    context = {
+        "projects": projects,
+    }
+    return render(request, "projects/list_projects.html", context)
--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_home(request):
+    return redirect("list_projects")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", redirect_to_home),
+    path("projects/", include("projects.urls"))
 ]
