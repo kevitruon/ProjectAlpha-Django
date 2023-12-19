9b12cbcea0d96bfdaba28537f078330a769df980
kevitruon <kevitruon@gmail.com>
2023-12-18 16:50:33 -0800

    Feature 13 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -13,3 +13,11 @@ def list_projects(request):
 
 def redirect_to_home(request):
     return redirect("list_projects")
+
+@login_required
+def show_project(request, id):
+    project = get_object_or_404(Project, id=id)
+    context = {
+        "project_object": project,
+    }
+    return render(request, "projects/show_project.html", context)
